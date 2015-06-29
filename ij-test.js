342b0862756c767e13231fcdcd815fe3757e8b79
Michaël Zasso <mic.besace@gmail.com>
2015-06-29 15:25:17 +0200

    add getHash

--- ij-test.js
+++ ij-test.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var path = require('path');
+var SHA256 = require('sha.js').sha256;
 
 exports.getImage = function getImage(name) {
     return path.resolve(__dirname, 'img', name);
+};
+
+exports.getHash = function getHash(img) {
+    var sha = new SHA256();
+    sha.update(img.data);
+    return sha.digest('hex');
 };
