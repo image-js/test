03d62d2ee629aeb31b0add92735f5e7d3f59370e
lpatiny <luc@patiny.com>
2015-09-15 13:30:48 +0200

    Update image list

--- ij-test.js
+++ ij-test.js
@@ -4,18 +4,28 @@ var path = require('path');
 var SHA256 = require('sha.js').sha256;
 
 exports.imageList = [
-    'cat.jpg',
-    'cat-blur.jpg',
-    'cells.jpg',
-    'ecoli.png',
-    'xtc.jpg',
-    'grey8.png',
-    'grey16.png',
-    'rgb8.png',
-    'rgb16.png',
     'BW11x11.png',
     'BW15x15.png',
-    'BW15x15transparent.png'
+    'BW15x15notTransparent.png',
+    'BW15x15transparent.png',
+    'BW2x2.png',
+    'BW3x3.png',
+    'BW4x4.png',
+    'BW5x5.png',
+    'BW7x7.png',
+    'BWGrid5x5.png',
+    'ColorGrid5x5.png',
+    'cat-blur.jpg',
+    'cat.jpg',
+    'cells.jpg',
+    'ecoli.png',
+    'grey16.png',
+    'grey8.png',
+    'isolateCells.png',
+    'rgb32bits.png',
+    'rgb64bits.png',
+    'rgb8bits.png',
+    'xtc.jpg'
 ];
 
 exports.getImage = function getImage(name) {
